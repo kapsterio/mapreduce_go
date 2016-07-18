74651016ae9e6adcd0b822daea1cee9acad330f0
zhangheng09 <zhangheng09@meituan.com>
2016-07-18 21:43:29 +0800

    handle failure

--- schedule.go
+++ schedule.go
@@ -6,39 +6,55 @@ import "fmt"
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
 	var nios int // number of inputs (for reduce) or outputs (for map)
+	//var taskArr []int
 	switch phase {
 	case mapPhase:
 		ntasks = len(mr.files)
 		nios = mr.nReduce
-
 	case reducePhase:
 		ntasks = mr.nReduce
 		nios = len(mr.files)
 	}
 
+	task := make(chan int)
+	go func() {
+		for index := 0; index < ntasks; index++ {
+			task <- index
+		}
+	}()
+
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
 
-	for index := 0; index < ntasks; index++ {
+	for id := range task {
 		workerAddr := <-mr.registerChannel
 		var args DoTaskArgs
-		args.File = mr.files[index]
+		args.File = mr.files[id]
 		args.JobName = mr.jobName
 		args.NumOtherPhase = nios
 		args.Phase = phase
-		args.TaskNumber = index
+		args.TaskNumber = id
 
 		go func() {
-			call(workerAddr, "Worker.DoTask", &args, new(struct{}))
-			mr.registerChannel <- workerAddr
+			suc := call(workerAddr, "Worker.DoTask", &args, new(struct{}))
+			if suc {
+				if args.TaskNumber == (ntasks - 1) {
+					close(task)
+				}
+				mr.registerChannel <- workerAddr
+			} else {
+				task <- args.TaskNumber
+
+			}
 		}()
 	}
 	// drain the registerChannel
+	/**
 	if phase == reducePhase {
 		for range mr.workers {
 			<-mr.registerChannel
 		}
 	}
-
+	*/
 	// All ntasks tasks have to be scheduled on workers, and only once all of
 	// them have been completed successfully should the function return.
 	// Remember that workers may fail, and that any given worker may finish
